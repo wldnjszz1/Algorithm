7622a7cb0ff0e855e424a792d0f9c28c7d8ff863
Jaelyn <47404465+wldnjszz1@users.noreply.github.com>
2019-03-27 22:20:13 +0900

    Delete programmers_hash_marathon1.py

--- programmers_hash_marathon1.py
+++
@@ -1,7 +0,0 @@
-def solution(participant, completion):
-    completion.append('z'*5)
-    completion.sort()
-    participant.sort()
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
